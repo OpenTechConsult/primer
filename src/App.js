78af2ec610306ff1e2c4db37aa88d4fdb3c225c0
Elom Atsou Agboka <opentech.consult@gmail.com>
2019-11-18 16:17:57 +0000

    Defining a function in the App.js File in the src Folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 const count = 4
 
+function isEven() {
+  return count % 2 === 0 ? "Even" : "Odd";
+}
+
 export default class App extends Component {
   render = () =>
     <h4 className="bg-primary text-white text-center p-2">
-      Number of things: { count % 2 === 0 ? "Even" : "Odd" }
+      Number of things: { isEven() }
     </h4>
 }
