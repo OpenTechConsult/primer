213a8c67c85c788a18c025b14392822e85befae1
Elom Atsou Agboka <opentech.consult@gmail.com>
2019-11-18 15:51:05 +0000

    Performing computation in the App.js file in the src folder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,6 @@ const count = 4
 export default class App extends Component {
   render = () =>
     <h4 className="bg-primary text-white text-center p-2">
-      Number of things: { count }
+      Number of things: { count % 2 === 0 ? "Even" : "Odd" }
     </h4>
 }
